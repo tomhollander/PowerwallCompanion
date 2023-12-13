225feb220634d677c71b001648e20de70a66a1ad
Tom Hollander <tomhollander@hotmail.com>
2023-12-13 19:18:19 +1100

    Tooltips, graph improvements

--- PowerwallCompanion/ChartPage.xaml.cs
+++ PowerwallCompanion/ChartPage.xaml.cs
@@ -433,6 +433,7 @@ namespace PowerwallCompanion
             }
         }
 
+
     }
 
 
---
+++ PowerwallCompanion/Converters/TooltipMessageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PowerwallCompanion.Converters
+{
+    internal class TooltipMessageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double doubleValue = (double)value / 1000;
+            return doubleValue.ToString("f2") + (string)parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
