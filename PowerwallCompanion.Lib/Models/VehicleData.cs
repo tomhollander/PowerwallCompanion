ef115d83b1aebc50d4ef36fde8492e42aca7c75f
Tom Hollander <tomhollander@hotmail.com>
2024-06-16 11:04:47 +1000

    Move to PWC.Lib

---
+++ PowerwallCompanion.Lib/Models/VehicleData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerwallCompanion.Lib.Models
+{
+    public class VehicleData
+    {
+        public string VehicleId { get; set; }
+        public string VehicleName { get; set; }
+        public int BatteryLevel { get; set; }
+        public bool IsAwake { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public DateTime LastWoken { get; set; }
+    }
+}
