6467e6be833da0080f33afa1a782f27bc809da3c
Tom Hollander <tomhollander@hotmail.com>
2025-02-09 09:01:48 +1100

    2.30

--- PowerwallCompanion.Lib/AzureFunctionsTelemetry.cs
+++ PowerwallCompanion.Lib/AzureFunctionsTelemetry.cs
@@ -1,6 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
-using PowerwallCompanion.Lib.Models;
+﻿using PowerwallCompanion.Lib.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
