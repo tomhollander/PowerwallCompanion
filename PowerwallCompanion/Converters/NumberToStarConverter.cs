a7858cd8e07c299ff1939eabd6b5e3206724ea97
Tom Hollander <tomhollander@hotmail.com>
2025-04-18 15:13:54 +1000

    Energy sources graph

---
+++ PowerwallCompanion/Converters/NumberToStarConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace PowerwallCompanion.Converters
+{
+    public class NumberToStarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int i = (int)value;
+            return new GridLength(i, GridUnitType.Star);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
